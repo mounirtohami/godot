f8ae80a7632101b6a13788a4cbcf4258dd49615e
Mounir Tohami <53877170+WhalesState@users.noreply.github.com>
2025-07-28 09:27:22 +0300

    Godot 2D CI/CD

--- modules/mono/glue/GodotSharp/GodotSharp/Compat.cs
+++ modules/mono/glue/GodotSharp/GodotSharp/Compat.cs
@@ -130,43 +130,6 @@ partial class GraphNode
         remove => DeleteRequest -= value;
     }
 }
-partial class ImporterMesh
-{
-    /// <inheritdoc cref="AddSurface(Mesh.PrimitiveType, Godot.Collections.Array, Godot.Collections.Array{Godot.Collections.Array}, Godot.Collections.Dictionary, Material, string, ulong)"/>
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    public void AddSurface(Mesh.PrimitiveType primitive, Godot.Collections.Array arrays, Godot.Collections.Array<Godot.Collections.Array> blendShapes, Godot.Collections.Dictionary lods, Material material, string name, uint flags)
-    {
-        AddSurface(primitive, arrays, blendShapes, lods, material, name, (ulong)flags);
-    }
-}
-
-
-partial class MeshInstance3D
-{
-    /// <inheritdoc cref="CreateMultipleConvexCollisions(MeshConvexDecompositionSettings)"/>
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    public void CreateMultipleConvexCollisions()
-    {
-        CreateMultipleConvexCollisions(settings: null);
-    }
-}
-
-partial class Node3D
-{
-    /// <inheritdoc cref="LookAt(Vector3, Nullable{Vector3}, bool)"/>
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    public void LookAt(Vector3 target, Nullable<Vector3> up)
-    {
-        LookAt(target, up, useModelFront: false);
-    }
-
-    /// <inheritdoc cref="LookAtFromPosition(Vector3, Vector3, Nullable{Vector3}, bool)"/>
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    public void LookAtFromPosition(Vector3 position, Vector3 target, Nullable<Vector3> up)
-    {
-        LookAtFromPosition(position, target, up, useModelFront: false);
-    }
-}
 
 partial class RenderingDevice
 {
